222b214e7f81783b3d29f9c41602fc2817bcb70d
PaulaStiglich <pstiglich@gmail.com>
2020-11-21 16:03:20 -0600

    not pretty, but the modal works now

--- index.js
+++ index.js
@@ -6,16 +6,10 @@
 // let modalTrigger = document.querySelectorAll('modal-trigger');
 // }
 
-open.addEventListener('click', () => {
-  modal_container.classList.add('show');
-})
 
-close.addEventListener('click', () => {
-  modal_container.classList.remove('show');
-})
 
 const open = document.getElementById('open');
-const modal_container = document.getElementById('modal-container');
+const modal_container = document.getElementById('modal_container');
 const close = document.getElementById('close');
 
 open.addEventListener('click', () => {
